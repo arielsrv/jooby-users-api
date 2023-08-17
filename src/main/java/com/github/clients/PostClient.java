94dd0cd4f018100b55cfa19226cb8d314136d561
ariel.pineiro <ariel.pineiro@iskaypet.com>
2023-08-17 09:48:32 -0300

    guava

--- src/main/java/com/github/clients/PostClient.java
+++ src/main/java/com/github/clients/PostClient.java
@@ -2,28 +2,32 @@ package com.github.clients;
 
 import com.github.core.http.Response;
 import com.github.core.http.RestClient;
+import com.github.model.PostDto;
 import com.github.model.responses.PostResponse;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.reactivex.rxjava3.core.Single;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class PostClient {
 
-    private final String baseUrl;
-    @Inject
-    RestClient restClient;
+	private final String baseUrl;
+	@Inject
+	RestClient restClient;
 
-    @Inject
-    public PostClient() {
-        this.baseUrl = "https://gorest.co.in/public/v2";
-    }
+	@Inject
+	public PostClient() {
+		this.baseUrl = "https://gorest.co.in/public/v2";
+	}
 
-    public Single<List<PostResponse>> GetPostByUserId(long userId) {
-        String apiUrl = "%s/users/%d/posts".formatted(this.baseUrl, userId);
+	public Single<List<PostResponse>> GetPostByUserId(long userId) {
+		String apiUrl = "%s/users/%d/posts".formatted(this.baseUrl, userId);
 
-        return this.restClient.GetSingle(apiUrl, PostResponse[].class)
-            .doOnSuccess(Response::VerifyOkOrFail).map(response -> List.of(response.GetData()));
-    }
+		return this.restClient.GetSingle(apiUrl, PostResponse[].class)
+			.doOnSuccess(Response::VerifyOkOrFail).map(response -> List.of(response.GetData()));
+	}
 }
--- src/main/java/com/github/services/UserService.java
+++ src/main/java/com/github/services/UserService.java
@@ -5,43 +5,62 @@ import com.github.clients.UserClient;
 import com.github.model.PostDto;
 import com.github.model.UserDto;
 import com.github.model.responses.PostResponse;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class UserService {
 
-    @Inject
-    public UserClient userClient;
+	@Inject
+	public UserClient userClient;
 
-    @Inject
-    public PostClient postClient;
+	@Inject
+	public PostClient postClient;
 
-    public Single<List<UserDto>> GetUsers() {
-        return this.userClient.GetUsers().flatMapObservable(Observable::fromIterable)
-            .flatMapSingle(userResponse -> {
-                UserDto userDto = new UserDto();
-                userDto.id = userResponse.id;
-                userDto.name = userResponse.name;
+	private final Cache<Long, List<PostDto>> postDtoCache = CacheBuilder.newBuilder()
+		.expireAfterWrite(1, TimeUnit.MINUTES).concurrencyLevel(getConcurrencyLevel())
+		.maximumSize(50).recordStats().build();
+
+	private static int getConcurrencyLevel() {
+		return Runtime.getRuntime().availableProcessors() - 1;
+	}
+
+	public Single<List<UserDto>> GetUsers() {
+		return this.userClient.GetUsers().flatMapObservable(Observable::fromIterable)
+			.flatMapSingle(userResponse -> {
+				UserDto userDto = new UserDto();
+				userDto.id = userResponse.id;
+				userDto.name = userResponse.name;
 				userDto.email = userResponse.email;
 
-                return this.postClient.GetPostByUserId(userDto.id).flatMap(postsResponse -> {
-                    List<PostDto> postDtos = new ArrayList<>();
-                    for (PostResponse postResponse : postsResponse) {
-                        PostDto postDto = new PostDto();
-                        postDto.id = postResponse.id;
-                        postDto.title = postResponse.title;
-						postDto.body = postResponse.body;
-                        postDtos.add(postDto);
-                    }
-                    userDto.posts = postDtos;
+				// guava poc, remove it
+				List<PostDto> postsDto = this.postDtoCache.getIfPresent(userDto.id);
 
-                    return Single.just(userDto);
-                });
-            }).collect(ArrayList::new, List::add);
-    }
+				if (postsDto != null) {
+					userDto.posts = postsDto;
+					return Single.just(userDto);
+				}
+
+				return this.postClient.GetPostByUserId(userDto.id).flatMap(postsResponse -> {
+					List<PostDto> postDtos = new ArrayList<>();
+					for (PostResponse postResponse : postsResponse) {
+						PostDto postDto = new PostDto();
+						postDto.id = postResponse.id;
+						postDto.title = postResponse.title;
+						postDto.body = postResponse.body;
+						postDtos.add(postDto);
+					}
+					userDto.posts = postDtos;
+					this.postDtoCache.put(userDto.id, postDtos);
+					return Single.just(userDto);
+				});
+			}).collect(ArrayList::new, List::add);
+	}
 }
