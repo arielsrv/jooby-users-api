c33ca692c9ea78e0eba3b7f1b3768103a98efa40
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-12-24 16:20:32 -0300

    fixes

--- src/main/java/com/github/core/http/RestClient.java
+++ src/main/java/com/github/core/http/RestClient.java
@@ -4,12 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.rxjava3.core.Single;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import java.io.IOException;
 import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import org.jetbrains.annotations.NotNull;
 
 @Singleton
 public class RestClient {
@@ -28,41 +25,9 @@ public class RestClient {
 		Request request = new Request.Builder().url(url).method(method, null).build();
 		return Single.create(emitter -> {
 			Call call = this.okHttpClient.newCall(request);
-			call.enqueue(new Callback() {
-				@Override
-				public void onFailure(@NotNull Call call, @NotNull IOException e) {
-					emitterOnError(e);
-				}
-
-				private void emitterOnError(Exception e) {
-					if (!emitter.isDisposed()) {
-						emitter.onError(e);
-					}
-				}
-
-				@Override
-				public void onResponse(@NotNull Call call, @NotNull okhttp3.Response response) {
-					try {
-						if (!response.isSuccessful()) {
-							throw new IOException("Unexpected HTTP response: " + response.code());
-						}
-
-						T unmarshal;
-						try (okhttp3.ResponseBody responseBody = response.body()) {
-							if (responseBody == null) {
-								throw new IOException("Response body is null");
-							}
-							unmarshal = objectMapper.readValue(responseBody.string(), clazz);
-						}
-
-						if (!emitter.isDisposed()) {
-							emitter.onSuccess(new Response<>(response.code(), unmarshal));
-						}
-					} catch (Exception e) {
-						emitterOnError(e);
-					}
-				}
-			});
+			RESTCallback<T> callback = new RESTCallback<>(emitter, this.objectMapper, clazz);
+			call.enqueue(callback);
 		});
 	}
 }
+
