67fedb3a43b549da954f5313b3ff61c77f07d827
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-12-24 16:23:22 -0300

    fix

--- src/main/java/com/github/core/http/RestClient.java
+++ src/main/java/com/github/core/http/RestClient.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.rxjava3.core.Single;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
@@ -18,15 +17,15 @@ public class RestClient {
 	public ObjectMapper objectMapper;
 
 	public <T> Single<Response<T>> get(String url, Class<T> clazz) {
-		return makeRequest("GET", url, clazz);
+		return createRequest("GET", url, clazz);
 	}
 
-	public <T> Single<Response<T>> makeRequest(String method, String url, Class<T> clazz) {
+	public <T> Single<Response<T>> createRequest(String method, String url, Class<T> clazz) {
 		Request request = new Request.Builder().url(url).method(method, null).build();
+
 		return Single.create(emitter -> {
-			Call call = this.okHttpClient.newCall(request);
-			RESTCallback<T> callback = new RESTCallback<>(emitter, this.objectMapper, clazz);
-			call.enqueue(callback);
+			this.okHttpClient.newCall(request)
+				.enqueue(new RestCallback<>(emitter, this.objectMapper, clazz));
 		});
 	}
 }
