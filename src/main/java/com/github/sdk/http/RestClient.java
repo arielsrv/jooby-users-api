9cccd9276e28f2e1e50705c94e48d361923b98c4
ariel.pineiro <ariel.pineiro@iskaypet.com>
2024-12-25 16:11:16 -0300

    named

--- src/main/java/com/github/sdk/http/RestClient.java
+++ src/main/java/com/github/sdk/http/RestClient.java
@@ -3,8 +3,11 @@ package com.github.sdk.http;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.rxjava3.core.Single;
 import jakarta.inject.Inject;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Request.Builder;
+import okhttp3.RequestBody;
 
 public class RestClient {
 
@@ -25,10 +28,31 @@ public class RestClient {
 		return createRequest("GET", url, clazz);
 	}
 
-	public <T> Single<Response<T>> createRequest(String method, String url, Class<T> clazz) {
-		String apiUrl = "%s%s".formatted(baseUrl, url);
+	public <B, T> Single<Response<T>> post(String url, B body, Class<T> clazz) {
+		return createRequest("POST", url, body, clazz);
+	}
 
-		Request request = new Request.Builder().url(apiUrl).method(method, null).build();
+	public <T> Single<Response<T>> createRequest(String method, String url,
+		Class<T> clazz) {
+		return createRequest(method, url, null, clazz);
+	}
+
+	public <B, T> Single<Response<T>> createRequest(String method, String url, B body,
+		Class<T> clazz) {
+		String apiUrl = "%s%s".formatted(this.baseUrl, url);
+
+		RequestBody requestBody = null;
+		if (body != null) {
+			try {
+				String json = this.objectMapper.writeValueAsString(body);
+				requestBody = RequestBody.create(json, MediaType.parse("application/json"));
+			} catch (Exception e) {
+				return Single.error(
+					new IllegalArgumentException("error serializing request body", e));
+			}
+		}
+
+		Request request = new Builder().url(apiUrl).method(method, requestBody).build();
 
 		return Single.create(emitter -> {
 			this.okHttpClient.newCall(request)
